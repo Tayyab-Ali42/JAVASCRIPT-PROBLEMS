984b5046b2cbe8c9a95bab53156d4d56d7c42486
Tayyab Ali <freelancewithtayyab@gmail.com>
2025-12-12 18:44:54 +0500

    Solve one more problem

--- Problems.js
+++ Problems.js
@@ -11,3 +11,39 @@ function toArray(obj) {
 console.log(toArray({ a: 1, b: 2 })) // ➞[["a", 1], ["b", 2]] 
 console.log(toArray({ shrimp: 15, tots: 12 })) // ➞[["shrimp", 15], ["tots", 12]]
 console.log(toArray({})) // ➞[]
+
+
+
+// PROBLEM 2 Calculate the Total Price of Groceries
+// Create a function that takes an array of objects(groceries) which calculates the total price and returns it as a number.A grocery object has a product, a quantity and a price, for example:
+
+
+function getTotalPrice(array) {
+    let TotalPrice = 0
+    array.forEach((item) => {
+        TotalPrice += item.quantity * item.price
+    })
+
+    return TotalPrice
+
+}
+
+console.log(getTotalPrice([
+    { product: "Milk", quantity: 1, price: 1.50 }
+]))// ➞ 1.5
+
+console.log(getTotalPrice([
+    { product: "Milk", quantity: 1, price: 1.50 },
+    { product: "Cereals", quantity: 1, price: 2.50 }
+]))// ➞ 4
+
+console.log(getTotalPrice([
+    { product: "Milk", quantity: 3, price: 1.50 }
+]))// ➞ 4.5
+
+console.log(getTotalPrice([
+    { product: "Milk", quantity: 1, price: 1.50 },
+    { product: "Eggs", quantity: 12, price: 0.10 },
+    { product: "Bread", quantity: 2, price: 1.60 },
+    { product: "Cheese", quantity: 1, price: 4.50 }
+]))// ➞ 10.4
