3de70a9f2334b9c4c8870aa7babd0d4e02f5174b
Tayyab Ali <freelancewithtayyab@gmail.com>
2025-12-13 16:13:25 +0500

    Solve another object problem

--- Problems.js
+++ Problems.js
@@ -65,3 +65,32 @@ console.log(getFrequencies(["A", "B", "A", "A", "A"]))// ➞ { A: 4, B: 1 }
 console.log(getFrequencies([1, 2, 3, 3, 2]))// ➞ { "1": 1, "2": 2, "3": 2 }
 console.log(getFrequencies([true, false, true, false, false]))// ➞ { true: 2, false: 3 }
 console.log(getFrequencies([]))// ➞ { }
+
+
+
+// PROBLEM 4 The Frugal Gentleman
+// Atticus has been invited to a dinner party, and he decides to purchase a bottle of wine.However, he has little knowledge of how to choose a good bottle.Being a very frugal gentleman(yet disliking looking like a cheapskate), he decides to use a very simple rule.In any selection of two or more wines, he will always buy the second - cheapest.
+
+// Given an array of wine objects, write a function that returns the name of the wine he will buy for the party.If given an empty array, return null.If given an array of only one, Atticus will buy that wine.
+
+
+function chosenWine(wines) {
+    if (!wines.length) return null
+    if (wines.length == 1) return wines[0].name
+
+    wines.sort((a, b) => a.price - b.price)
+
+    return wines[1].name
+
+}
+
+
+
+console.log(chosenWine([
+    { name: "Wine A", price: 8.99 },
+    { name: "Wine 32", price: 13.99 },
+    { name: "Wine 9", price: 10.99 }
+]))
+
+console.log(chosenWine([{ name: "Wine A", price: 8.99 }]))// ➞ "Wine A"
+console.log(chosenWine([]))// ➞ null
