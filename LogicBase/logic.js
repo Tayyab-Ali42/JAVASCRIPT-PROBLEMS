b205e38787bba95644cc65344128b12b2ca5d5c0
Tayyab Ali <freelancewithtayyab@gmail.com>
2025-12-24 23:32:20 +0500

    .

--- LogicBase/logic.js
+++ LogicBase/logic.js
@@ -48,3 +48,52 @@ taskTracker.addTask('Hello', new Date("2025-12-20"), 5)
 taskTracker.addTask('Hello2 ', new Date("2025-12-20"), 4)
 taskTracker.markDone(2)
 console.log(taskTracker.listTasks())
+
+
+
+
+
+// REAL WORLD PROBLEM 1 Event Booking Conflict & Capacity Manager
+
+// Real - World Context
+
+// You are building the core booking logic for an event management system(like conference rooms, workshops, or classes).
+// Each event:
+
+// Has a start time
+// Has an end time
+// Has a capacity
+// Receives booking requests over time
+// Your job is to process booking requests correctly and efficiently.
+
+let processBookings = (events, requests) => {
+
+    const eventMap = new Map()
+    for (const e of eventMap) {
+        eventMap.set(e.id, {
+            ...e,
+            start: 0,
+            end: 0
+        })
+    }
+}
+
+
+
+
+
+
+const events = [
+    { id: 1, start: "09:00", end: "10:30", capacity: 2 },
+];
+const requests = [
+    { eventId: 1, userId: "A" },
+    // { eventId: 1, userId: "B" },
+    // { eventId: 1, userId: "C" }, // should fail (capacity exceeded)
+    // { eventId: 2, userId: "A" },
+    // { eventId: 2, userId: "B" }, // should fail
+    // { eventId: 3, userId: "A" },
+    // { eventId: 3, userId: "A" }  // duplicate booking (should fail)
+];
+
+console.log(processBookings(events, requests))
