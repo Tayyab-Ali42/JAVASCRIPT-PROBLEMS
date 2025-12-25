ad196ff2f856f950841c26861a05080b2e854a8c
Tayyab Ali <freelancewithtayyab@gmail.com>
2025-12-25 17:07:12 +0500

    `Added new functions and tests to Arrays.js file`

--- ARRAYS/Arrays.js
+++ ARRAYS/Arrays.js
@@ -14,11 +14,11 @@ console.log(sevenBoom([2, 55, 60, 97, 86, 90]))
 // PROBLEM 2 How Much is True?
 // Create a function which returns the number of true values there are in an array.
 
-// let countTrue = arr => arr.filter(word => word === true).length
+let countTrue = arr => arr.filter(word => word === true).length
 
-// console.log(countTrue([true, false, false, true, false]))
-// console.log(countTrue([false, false, false, false]))
-// console.log(countTrue([]))
+console.log(countTrue([true, false, false, true, false]))
+console.log(countTrue([false, false, false, false]))
+console.log(countTrue([]))
 
 
 
@@ -61,8 +61,16 @@ const changeEnough = (coins, price) => {
     return total >= price * 100
 }
 console.log(changeEnough([2, 100, 0, 0], 14.11))
-console.log(changeEnough([0, 0, 20, 5], 0.75))
-console.log(changeEnough([30, 40, 20, 5], 12.55))
 console.log(changeEnough([10, 0, 0, 50], 3.85))
-console.log(changeEnough([1, 0, 5, 219], 19.99))
-console.log(changeEnough([25, 20, 5, 0], 4.25))
+
+
+// PROBLEM 6 Array of Multiples
+// Create a function that takes two numbers as arguments (num, length) and returns an array of multiples of num until the array length reaches length.
+
+
+let arrayOfMultiples = (num, length) => {
+    return [...Array(length)].map((_, i) => num * (i + 1))
+}
+
+console.log(arrayOfMultiples(7, 5))
+console.log(arrayOfMultiples(12, 10))
